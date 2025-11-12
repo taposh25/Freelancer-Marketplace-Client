7b31240cb3fd42db748ff09f83eab02e573a2e36
Taposh Barai <taposhbarai03@gmail.com>
2025-11-12 22:30:06 +0600

    navbar-added

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,9 +1,4 @@
-// import React from "react";
-// import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer";
-// import { Outlet } from "react-router-dom";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
