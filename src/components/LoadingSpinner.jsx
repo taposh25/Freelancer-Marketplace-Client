cb70ee0c98155cd8dc494308f18b91a6765c9d9b
Taposh Barai <taposhbarai03@gmail.com>
2025-11-12 22:31:49 +0600

    loading-spionner-added

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadingSpinner = () => (
-  <div className="flex justify-center items-center h-64">
+  <div className="flex justify-center items-center h-65">
     <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-blue-500 border-t-transparent"></div>
   </div>
 );
