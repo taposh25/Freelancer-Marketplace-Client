9876645aac429e087a1ba51fe7e74f158142fe5d
Taposh Barai <taposhbarai03@gmail.com>
2025-11-13 12:27:31 +0600

    all-jobs-added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -147,6 +147,7 @@ const Home = () => {
   return (
     <div className="container mx-auto p-6 ">
       <h1 className="text-3xl font-bold mb-6 ">Available Jobs</h1>
+      
       {jobs.length === 0 && <p className="text-center text-gray-500">No jobs available.</p>}
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
--- src/Pages/Jobs/AddJob.jsx
+++ src/Pages/Jobs/AddJob.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-// import { useNavigate } from "react-router-dom";
-// import { toast } from "react-toastify";
+
 
 const AddJob = () => {
   const navigate = useNavigate();
